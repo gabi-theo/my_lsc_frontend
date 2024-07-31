fb5fa6cb89d05d30d676f977de79545db1ced4d7
Alex Suciu <v.alex.suciu@gmail.com>
2024-07-31 16:09:05 +0300

    Added new calendar design

--- home.js
+++ home.js
@@ -1,13 +1,18 @@
 import { navbar } from "./components/navbar.js";
 import { generateCards } from "./components/card.js";
 import { userLogout } from "./components/logout.js";
-import { addDate } from "./components/calendar.js";
-import { fetchData } from "./components/calendar.js";
+// import { addDate } from "./components/calendar.js";
+// import { fetchData } from "./components/calendar.js";
+import {setInitialDates, changeDates, fetchData, displayEvents} from "./components/calendarNew.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   navbar();
   generateCards();
-  addDate();
+  // addDate();
   fetchData();
   userLogout();
+  setInitialDates();
+  changeDates();
+  displayEvents();
+  
 });
